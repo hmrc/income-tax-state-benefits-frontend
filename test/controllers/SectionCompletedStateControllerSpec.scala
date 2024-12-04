c5e5a1c2565cb5433bc8cb97e8b5cf6b622a9468
luke.jones <47660116+BetaDraconis@users.noreply.github.com>
2024-12-04 19:49:06 +0000

    SASS-10602: Fix?

--- test/controllers/SectionCompletedStateControllerSpec.scala
+++ test/controllers/SectionCompletedStateControllerSpec.scala
@@ -16,7 +16,7 @@
 
 package controllers
 
-import config.AppConfig
+import config.{AppConfig, AppConfigImpl}
 import forms.YesNoForm
 import models.BenefitType.JobSeekersAllowance
 import models.Done
@@ -74,7 +74,7 @@ class SectionCompletedStateControllerSpec extends AnyFreeSpec with MockitoSugar 
       ConfidenceLevel.L250
     )
 
-  private val mockAppConfig = mock[AppConfig]
+  private val mockAppConfig = mock[AppConfigImpl]
   private val mockAuthConnector = mock[AuthConnector]
 
   when(mockAuthConnector.authorise[Option[AffinityGroup]](any(), eqTo(affinityGroup))(any(), any()))
@@ -96,10 +96,8 @@ class SectionCompletedStateControllerSpec extends AnyFreeSpec with MockitoSugar 
   "SectionCompletedStateController Controller" - {
 
     "must return OK and the correct view for a GET" in {
-
       val application = new GuiceApplicationBuilder()
         .overrides(
-          bind[AppConfig].toInstance(mockAppConfig),
           bind[AuthConnector].toInstance(mockAuthConnector),
           bind[SectionCompletedService].toInstance(mockService)
         )
