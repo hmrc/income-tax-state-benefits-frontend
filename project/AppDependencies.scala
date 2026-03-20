997a75950076324ceaff4b0fc68ab412bf3c7dbb
nadinJ <63252254+nadinJ@users.noreply.github.com>
2026-03-20 14:46:09 +0000

    MIPR-2415

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -18,7 +18,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapPlay30Version = "10.6.0"
+  private val bootstrapPlay30Version = "10.7.0"
   private val hmrcMongoPlay30Version = "2.12.0"
 
   val jacksonAndPlayExclusions: Seq[InclusionRule] = Seq(
@@ -34,8 +34,8 @@ object AppDependencies {
     "uk.gov.hmrc"                   %% "play-frontend-hmrc-play-30" % "12.32.0",
     "uk.gov.hmrc.mongo"             %% "hmrc-mongo-play-30"         % hmrcMongoPlay30Version,
     "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.21.1",
-    "com.beachape"                  %% "enumeratum"                 % "1.9.5",
-    "com.beachape"                  %% "enumeratum-play-json"       % "1.9.5" excludeAll (jacksonAndPlayExclusions *)
+    "com.beachape"                  %% "enumeratum"                 % "1.9.6",
+    "com.beachape"                  %% "enumeratum-play-json"       % "1.9.6" excludeAll (jacksonAndPlayExclusions *)
   )
 
   val test: Seq[ModuleID] = Seq(
@@ -45,6 +45,6 @@ object AppDependencies {
     "com.github.tomakehurst"  %  "wiremock-jre8-standalone"   % "3.0.1",
     "org.scalamock"           %% "scalamock"                  % "7.5.5",
     "com.vladsch.flexmark"    %  "flexmark-all"               % "0.64.8",
-    "org.mockito"             %% "mockito-scala"              % "2.0.0"
+    "org.mockito"             %% "mockito-scala"              % "2.1.0"
   ).map(_ % Test)
 }
